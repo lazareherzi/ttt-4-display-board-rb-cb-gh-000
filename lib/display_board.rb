f27ec3812627616b72eac036df26fbd407bc5f6a
Lazare Herzi <lazareherzi@gmail.com>
2017-02-18 02:48:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  def even_line()
+    puts "-----------"
+  end
+
+
+  puts " #{ board[0]} | #{board[1]} | #{board[2]} "
+  even_line
+  puts " #{ board[3]} | #{board[4]} | #{board[5]} "
+  even_line
+  puts " #{ board[6]} | #{board[7]} | #{board[8]} "
+end
